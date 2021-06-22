d1d2b199704048966987a2625c8e6dddef76fb3b
arunm5341 <84780164+arunm5341@users.noreply.github.com>
2021-06-22 14:46:30 +0530

    Update TestQuestion1.scala

--- src/test/scala/TestQuestion1.scala
+++ src/test/scala/TestQuestion1.scala
@@ -1,12 +1,7 @@
 
-import org.scalatest._
+import org.scalatest.flatspec._
 
-class TestQuestion1 extends AnyFunSuite {
-  test("TestSample") {
-    assert(Question1.greater(1,2)===2)
-    assert(Question1.maximum((1, 2, 3),greater) === 3)
-  }
-}
+
 class TestQuestion1 extends AnyFlatSpec {
   it should "match" in {
     assert(  3 == Question1.greater(3,2))
