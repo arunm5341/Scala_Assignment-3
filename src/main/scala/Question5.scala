35a44c448ccf764886c8193a9a9942188b94f486
arunm5341 <84780164+arunm5341@users.noreply.github.com>
2021-06-22 12:41:07 +0530

    new function added

--- src/main/scala/Question5.scala
+++ src/main/scala/Question5.scala
@@ -2,10 +2,7 @@ object Question5 extends App {
   def conditional[A](x: A, p: A => Boolean, f: A => A) = {
     if (p(x)) f(x) else x
   }
-  val a = conditional[String]("aRYUn", _.size > 2, _.reverse)
-  val a1 = conditional[Int](45, _ % 3 == 0, _ =>4)
-println(a1,a)
-
+  
 }
 
 
