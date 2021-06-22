54d8296ec985e3af60b72d9a1320d0c91a5b40ce
arunm5341 <84780164+arunm5341@users.noreply.github.com>
2021-06-22 12:30:47 +0530

    updated two new conditions

--- src/test/scala/TestQuestion5.scala
+++ src/test/scala/TestQuestion5.scala
@@ -3,6 +3,7 @@ import org.scalatest.funsuite.AnyFunSuite
 class TestQuestion5 extends AnyFunSuite{
   test("TestQuestion5"){
     assert("nUYRa"===Question5.conditional[String]("aRYUn", _.size > 2, _.reverse))
+    assert(4===Question5.conditional[Int](45, _ % 3 == 0, _ =>4))
 
   }
 
