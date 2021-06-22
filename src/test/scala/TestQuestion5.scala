6f5fe6dbd7d84b900c64cef24ce28a7c08507042
arunm5341 <84780164+arunm5341@users.noreply.github.com>
2021-06-22 12:27:15 +0530

    Update TestQuestion5.scala

--- src/test/scala/TestQuestion5.scala
+++ src/test/scala/TestQuestion5.scala
@@ -2,6 +2,7 @@ import org.scalatest.funsuite.AnyFunSuite
 
 class TestQuestion5 extends AnyFunSuite{
   test("TestQuestion5"){
+    assert("nUYRa"===Question5.conditional[String]("aRYUn", _.size > 2, _.reverse))
 
   }
 
